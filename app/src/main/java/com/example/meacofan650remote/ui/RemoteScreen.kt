c128d27f21136bbee2699cd3f3a609193d69e899
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-11 03:10:58 +0100

    Centered remote UI.

--- app/src/main/java/com/example/meacofan650remote/ui/RemoteScreen.kt
+++ app/src/main/java/com/example/meacofan650remote/ui/RemoteScreen.kt
@@ -1,9 +1,11 @@
 package com.example.meacofan650remote.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -24,50 +26,61 @@ fun RemoteScreen(
     modifier: Modifier = Modifier,
     paddingValues: PaddingValues = PaddingValues()
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(paddingValues)
-    )
-    {
-        SpeedWidget()
+    Box(modifier = modifier
+        .padding(paddingValues)
+        .padding(horizontal = 24.dp)
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            SpeedWidget()
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
 
-            RemoteButton(icon = Icons.Outlined.Eco, text = "Eco", modifier = Modifier.weight(1f)) {
+                RemoteButton(icon = Icons.Outlined.Eco, text = "Eco", modifier = Modifier.weight(1f)) {
+
+                }
+
+
+                RemoteButton(icon = Icons.Outlined.RotateLeft, text = "Oscillate", modifier = Modifier.weight(1f)) {
+
+                }
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                RemoteButton(icon = Icons.Outlined.BrightnessMedium, text = "Light", modifier = Modifier.weight(1f)) {
+
+                }
+
+                RemoteButton(icon = Icons.Outlined.VolumeOff, text = "Mute", modifier = Modifier.weight(1f)) {
+
+                }
+
 
             }
 
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                RemoteButton(icon = Icons.Outlined.Timer, text = "Timer", modifier = Modifier.weight(1f)) {
 
-            RemoteButton(icon = Icons.Outlined.RotateLeft, text = "Oscillate", modifier = Modifier.weight(1f)) {
+                }
 
+                RemoteButton(icon = Icons.Outlined.PowerSettingsNew, text = "Power", emphasise = true, modifier = Modifier.weight(1f)) {
+
+                }
             }
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            RemoteButton(icon = Icons.Outlined.BrightnessMedium, text = "Light", modifier = Modifier.weight(1f)) {
-
-            }
-
-            RemoteButton(icon = Icons.Outlined.VolumeOff, text = "Mute", modifier = Modifier.weight(1f)) {
-
-            }
-
-            RemoteButton(icon = Icons.Outlined.Timer, text = "Timer", modifier = Modifier.weight(1f)) {
-
-            }
-
-
-        }
-
-        RemoteButton(icon = Icons.Outlined.PowerSettingsNew, text = "Power", emphasise = true, modifier = Modifier.fillMaxWidth()) {
-
         }
     }
 
