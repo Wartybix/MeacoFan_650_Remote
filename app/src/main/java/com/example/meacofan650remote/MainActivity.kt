b687ed53cd292b0f05c0c886dce78b0239ac1e4f
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-11 18:11:36 +0100

    Optimised imports.

--- app/src/main/java/com/example/meacofan650remote/MainActivity.kt
+++ app/src/main/java/com/example/meacofan650remote/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meacofan650remote.ui.OverallScreen
-import com.example.meacofan650remote.ui.RemoteScreen
-import com.example.meacofan650remote.ui.TopAppBar
 import com.example.meacofan650remote.ui.theme.MeacoFan650RemoteTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/meacofan650remote/ui/UnsupportedScreen.kt
+++ app/src/main/java/com/example/meacofan650remote/ui/UnsupportedScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Error
 import androidx.compose.material.icons.outlined.ErrorOutline
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,8 +19,8 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun UnsupportedScreen(
-    paddingValues: PaddingValues = PaddingValues(),
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues()
 ) {
     Column(
         modifier = modifier
