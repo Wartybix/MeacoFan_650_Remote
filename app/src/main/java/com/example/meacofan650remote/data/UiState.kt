bcb6e7666b329f0c5e8b2ad481503fb2a1ef220e
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-05-11 17:33:10 +0100

    Added viewmodel and UI State with skeleton broadcastSignal function.

---
+++ app/src/main/java/com/example/meacofan650remote/data/UiState.kt
@@ -0,0 +1,5 @@
+package com.example.meacofan650remote.data
+
+data class UiState(
+    var irSupported: Boolean = false
+)
--- app/src/main/java/com/example/meacofan650remote/ui/ViewModel.kt
+++ app/src/main/java/com/example/meacofan650remote/ui/ViewModel.kt
@@ -1,4 +1,30 @@
-package com.example.meacofan650remote.ui;
+package com.example.meacofan650remote.ui
 
-public class ViewModel {
+import android.app.Application
+import android.content.Context
+import android.hardware.ConsumerIrManager
+import androidx.lifecycle.AndroidViewModel
+import com.example.meacofan650remote.data.RemoteFunction
+import com.example.meacofan650remote.data.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ViewModel(application: Application) : AndroidViewModel(application) {
+    val context = application.applicationContext
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    val irManager: ConsumerIrManager = context.getSystemService(
+        Context.CONSUMER_IR_SERVICE
+    ) as ConsumerIrManager
+
+    init {
+        _uiState.value.irSupported = irManager.hasIrEmitter()
+    }
+
+    fun broadcastSignal(function: RemoteFunction) {
+
+    }
 }
